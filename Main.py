c484d2fc8d53f71d6c0628616a944be3cecf63a8
Codeveen <praveenkumaroffcal@gmail.com>
2024-05-17 15:41:28 -0400

    make it better

---
+++ Main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route("/propose", methods=['POST'])
+def propose():
+    return render_template('propose.html')
+
+@app.route("/final", methods=['POST'])
+def final():
+    return render_template('final.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
